95b0ea170d391f26c7911be6d8e2ed8536406956
perdreauarthur <arthur.perdreau@outlook.fr>
2025-05-14 17:02:17 +0200

    add carrousel for posts

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,6 +11,7 @@ final class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        return $this->render('home/index.html.twig');
         return $this->redirectToRoute('app_login');
     }
 }
