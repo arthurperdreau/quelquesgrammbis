42cd32e1481f89f2f92675e582bb98c44920daed
perdreauarthur <arthur.perdreau@outlook.fr>
2025-05-14 15:34:38 +0200

    add message fonctionnality

---
+++ src/Controller/ChatController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Conversation;
+use App\Entity\Message;
+use App\Entity\Profile;
+use App\Form\MessageForm;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ChatController extends AbstractController
+{
+    #[Route('/chat/new/{id}', name: 'app_chat_create')]
+    public function createChat(Profile $profile, EntityManagerInterface $manager): Response
+    {
+        if (!$this->getUser()){
+            return $this->redirectToRoute('app_login');
+        }
+        if(!$profile){return $this->redirectToRoute('app_myposts');}
+
+        $conversation = $this->getUser()->getProfile()->isChattingWith($profile);
+
+        if(!$conversation){
+            $conversation = new Conversation();
+            $conversation->addParticipant($this->getUser()->getProfile());
+            $conversation->addParticipant($profile);
+            $conversation->setCreatedAt(new \DateTimeImmutable());
+
+            $manager->persist($conversation);
+            $manager->flush();
+        }
+
+
+        return $this->redirectToRoute('app_chat', ['id' => $conversation->getId() ]);
+    }
+
+    #[Route('/chat/{id}', name: 'app_chat')]
+    public function chat(Conversation $chat, Request $request, EntityManagerInterface $manager): Response
+    {
+        if (!$this->getUser()){
+            return $this->redirectToRoute('app_login');
+        }
+        if(!$chat){return $this->redirectToRoute('app_myposts');}
+
+        $message = new Message();
+        $form = $this->createForm(MessageForm::class, $message);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+
+            $message->setAuthor($this->getUser()->getProfile());
+            $message->setConversation($chat);
+            $message->setCreatedAt(new \DateTimeImmutable());
+            $manager->persist($message);
+            $manager->flush();
+            return $this->redirectToRoute('app_chat', ['id' => $chat->getId()]);
+        }
+
+
+
+        return $this->render('chat/index.html.twig', [
+            'chat' => $chat,
+            'form' => $form,
+        ]);
+    }
+}
--- src/Entity/Profile.php
+++ src/Entity/Profile.php
@@ -52,6 +52,18 @@ class Profile
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $bio = null;
 
+    /**
+     * @var Collection<int, Message>
+     */
+    #[ORM\OneToMany(targetEntity: Message::class, mappedBy: 'author')]
+    private Collection $messages;
+
+    /**
+     * @var Collection<int, Conversation>
+     */
+    #[ORM\ManyToMany(targetEntity: Conversation::class, mappedBy: 'participants')]
+    private Collection $conversations;
+
 
     public function __construct()
     {
@@ -59,6 +71,8 @@ class Profile
         $this->friendAsPersonB = new ArrayCollection();
         $this->SentFriendRequests = new ArrayCollection();
         $this->receiveFriendRequests = new ArrayCollection();
+        $this->messages = new ArrayCollection();
+        $this->conversations = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -277,5 +291,70 @@ class Profile
         return $this;
     }
 
+    /**
+     * @return Collection<int, Message>
+     */
+    public function getMessages(): Collection
+    {
+        return $this->messages;
+    }
+
+    public function addMessage(Message $message): static
+    {
+        if (!$this->messages->contains($message)) {
+            $this->messages->add($message);
+            $message->setAuthor($this);
+        }
+
+        return $this;
+    }
+
+    public function removeMessage(Message $message): static
+    {
+        if ($this->messages->removeElement($message)) {
+            // set the owning side to null (unless already changed)
+            if ($message->getAuthor() === $this) {
+                $message->setAuthor(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Conversation>
+     */
+    public function getConversations(): Collection
+    {
+        return $this->conversations;
+    }
+
+    public function addConversation(Conversation $conversation): static
+    {
+        if (!$this->conversations->contains($conversation)) {
+            $this->conversations->add($conversation);
+            $conversation->addParticipant($this);
+        }
+
+        return $this;
+    }
+
+    public function removeConversation(Conversation $conversation): static
+    {
+        if ($this->conversations->removeElement($conversation)) {
+            $conversation->removeParticipant($this);
+        }
+
+        return $this;
+    }
+    public function isChattingWith(Profile $profile):Conversation | bool
+    {
+        foreach ($this->conversations as $conversation) {
+            if($conversation->getParticipants()->contains($profile)){
+                return $conversation;
+            }
+        }
+        return false;
+    }
 
 }
