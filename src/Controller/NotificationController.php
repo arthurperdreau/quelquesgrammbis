41401317ea947ce958f620fc989ebd0c00acc7a6
perdreauarthur <arthur.perdreau@outlook.fr>
2025-05-19 11:09:00 +0200

    add some file

--- src/Controller/NotificationController.php
+++ src/Controller/NotificationController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Notification;
 use App\Repository\NotificationRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,8 +14,23 @@ final class NotificationController extends AbstractController
     #[Route('/notification', name: 'app_notification')]
     public function index(NotificationRepository $notificationRepository): Response
     {
+        if(!$this->getUser()){
+            return $this->redirectToRoute('app_login');
+        }
         return $this->render('notification/index.html.twig', [
             'notifs' =>$notificationRepository-> findBy(['notified' => $this->getUser()->getProfile(), 'seen' => false]),
         ]);
+    }
+
+    #[Route('/notification/{id}/seen', name: 'app_notification_seen')]
+    public function hideNotification(Notification $notification, EntityManagerInterface $manager): Response
+    {
+        if(!$this->getUser()){
+            return $this->redirectToRoute('app_login');
+        }
+        $notification->setSeen(true);
+        $manager->flush();
+        return $this->redirectToRoute('app_notification');
+
     }
 }
